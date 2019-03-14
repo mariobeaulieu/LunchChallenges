91564e463081aafe5c910019723c89c34714c5e2
Mario Beaulieu <mabeaulieu@blackberry.com>
2019-03-14 11:27:39 -0400

    Completed fix of stream input for mlrose and manual optimization. Manual is better but uses more coding. mlrose crashes with 1000 dataset

--- travellingSalesman/TSP_Fitness_Distance.py
+++ travellingSalesman/TSP_Fitness_Distance.py
@@ -4,33 +4,43 @@ import sys
 import mlrose
 import numpy as np
 from sys import stdin
+print 'If program is stuck here, press ^D'
 
 nb=0
-coordsi_list=[]
+coords_list=[]
 try:
   for line in stdin:
     if line.strip() != '':
       coordTxt = line.split(' ')
       coords_list.append([int(coordTxt[0]),int(coordTxt[1])])
       nb+=1
+    else:
+      break
 except Exception as e:
   print 'Error when reading data stream: e'
+  sys.exit(1)
 
 if nb==0:
   print 'Usage: %s < infile '%(sys.argv[0])
-  sys.exit(1)
+  print '\nThe default dataset will be used'
+  # Coordinates of cities to visit
+  coords_list = [[1,1],[4,2],[5,2],[6,4],[4,4],[3,6],[1,5],[2,3]]
+  nb = len(coords_list)
+
+print '\n',nb,'values are in the set\n'
 
 
 # Coordinates of cities to visit
 #cities      = [ 'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H' ]
 #coords_list = [(1,1),(4,2),(5,2),(6,4),(4,4),(3,6),(1,5),(2,3)]
 nb_cities   = nb
-use_coords  = True
-
+use_coords = len(sys.argv)>1
 if use_coords:
+  print 'An argument was provided, so coordinates (not distances) will be used'
   # Initialize fitness function object using coord_list
   fitness_function = mlrose.TravellingSales(coords = coords_list)
 else:
+  print 'No argument was provided, so distances (not coordinates) will be used'
   # Create a distance list between pairs of cities
   dist_list   = []
   for i in range(nb_cities):
@@ -39,7 +49,7 @@ else:
   # Initialize fitness function object using dist_list
   fitness_function = mlrose.TravellingSales(distances = dist_list)
 
-problem_fit = mlrose.TSPOpt(length=nb_cities, fitness_fn=fitness_function, maximize=False)
+problem_fit = mlrose.TSPOpt(length=nb_cities, fitness_fn=fitness_function, maximize=True)
 
 # Solve the problem using the genetic algorithm
 best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state=2)
--- travellingSalesman/tsp_mario.py
+++ travellingSalesman/tsp_mario.py
@@ -4,33 +4,41 @@ import math
 import sys
 from sys import stdin
 
+RNDMAX=10000 # Max range for city coordinate
 nb=0
 coords=[]
+print 'If program is stuck here, press ^D'
 try:
   for line in stdin:
     if line.strip() != '':
       coordTxt = line.split(' ')
       coords.append([int(coordTxt[0]),int(coordTxt[1])])
       nb+=1
+    else:
+      break
 except Exception as e:
   print 'Error when reading data stream: e'
+  sys.exit(1)
 
 if nb==0:
   print 'Usage: %s < infile '%(sys.argv[0])
-  sys.exit(1)
+  print 'The default dataset will be used'
+  # Coordinates of cities to visit
+  coords    = [[1,1],[4,2],[5,2],[6,4],[4,4],[3,6],[1,5],[2,3]]
+  nb = len(coords)
 
-cities    = [str(i) for i in range(nb)]
+print '\n',nb,'values are in the set\n'
 
-# Coordinates of cities to visit
-#cities    = [ 'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H' ]
-#coords    = [[1,1],[4,2],[5,2],[6,4],[4,4],[3,6],[1,5],[2,3]]
-minDist   = 1000
+cities    = [i for i in range(nb)]
+attempts  = max(int(nb/10),3) # Number of attempts for each section
+
+minDist   = RNDMAX*nb
 minPath   = []
 
 distances = {}
 for i in range(nb):
     for j in range(i+1,nb):
-       distances[cities[i]+cities[j]] = math.sqrt( (coords[i][0]-coords[j][0])**2 + (coords[i][1]-coords[j][1])**2 )
+       distances[cities[i]*RNDMAX+cities[j]] = math.sqrt( (coords[i][0]-coords[j][0])**2 + (coords[i][1]-coords[j][1])**2 )
 
 numIterations = 100
 if sys.argv > 1:
@@ -52,20 +60,27 @@ for iteration in range(numIterations):
   city=0
   currPath.append(cities[city])
   used[0]=True
-  # range isnb-2 because we start with a fixed city
+  # range is nb-1 because we start with a fixed city
   for n in range(1,nb):
-    j=random.randint(1,nb-n)
-    while used[j]:
-      j=(j+1)%nb
-    used[j]=True
-    currPath.append(cities[j])
-    route = sorted([cities[city],cities[j]])
-    dist += distances[route[0]+route[1]]
-    city=j
+    bestDist=RNDMAX**2
+    bestj   =0
+    for attempt in range(attempts):
+      j=random.randint(1,nb-1)
+      while used[j]:
+        j=(j+1)%nb
+      route = sorted([cities[city],cities[j]])
+      d     = distances[route[0]*RNDMAX+route[1]]
+      if d<bestDist:
+        bestDist=d
+        bestj   =j
+    used[bestj]=True
+    currPath.append(cities[bestj])
+    dist += d
+    city=bestj
   # Then we link back to start city
   currPath.append(cities[0])
   route = sorted([cities[city],cities[0]])
-  dist += distances[route[0]+route[1]]
+  dist += distances[route[0]*RNDMAX+route[1]]
   if dist < minDist:
     minPath = currPath
     minDist=dist
